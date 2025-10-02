910784d4d4f865e0c75071cf1b2fb05e7502cee7
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2025-10-02 11:28:05 -0700

    add PIN_LED definition for automato_ brain

--- src/variants/automato_brain/pins_arduino.h
+++ src/variants/automato_brain/pins_arduino.h
@@ -8,6 +8,7 @@
 static const uint8_t LED_BUILTIN = 23;
 #define BUILTIN_LED LED_BUILTIN  // backward compatibility
 #define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
+#define PIN_LED LED_BUILTIN
 
 static const uint8_t TX = 16;
 static const uint8_t RX = 17;
